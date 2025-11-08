fb93b6c804424979543376eb99b9d222372f1cab
Anushka-Mandal <165414511+Anushka-Mandal@users.noreply.github.com>
2025-11-08 21:15:31 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root', // Replace with your MySQL username
-    password: 'Bidisha@2023', // Replace with your MySQL password
+    password: '', // Replace with your MySQL password
     database: 'rental_management',
     waitForConnections: true,
     connectionLimit: 10,
